9c9bd0cd9239dcf7bff25f7cae86749c4728c01a
Cheryl Ng <cherylngql@gmail.com>
2018-09-02 11:46:45 -0400

    connect store

--- client/index.js
+++ client/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App'
+import store from './store'
 
 ReactDOM.render(
-  <App />
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
   ,
   document.getElementById('app')
 )
