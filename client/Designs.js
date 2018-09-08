38bb3fdd38f480a4d4161711061e037a2b39cb51
Cheryl Ng <cherylngql@gmail.com>
2018-09-08 12:49:37 -0400

    show loading

--- client/Designs.js
+++ client/Designs.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Designs = () => {
-  return (
-    <div id="designs">
-      <div className="singleDesign">
-        <img className="design" src="lovebook.jpg"/>
+class Designs extends Component {
+  constructor(props) {
+    super(props)
+    this.onLoad = this.onLoad.bind(this)
+  }
+  onLoad(event) {
+    event.target.style.display = "block"
+    event.target.nextElementSibling.style.display = "none"
+  }
+  render() {
+    return (
+      <div id="designs">
+        <div className="singleDesign">
+          <img onLoad={this.onLoad} className="design" id="lovebook" src="lovebook.jpg"/>
+          <div className="loading">loading fabulous artwork...</div>
+        </div>
+        <div className="singleDesign">
+          <img onLoad={this.onLoad} className="design" id="intempo" src="INTEMPO.jpg"/>
+          <div className="loading">loading extraordinary design...</div>
+        </div>
+        <div className="singleDesign">
+          <img onLoad={this.onLoad} className="design" id="dacapo" src="DACAPO.jpg"/>
+          <div className="loading">loading breath-taking piece...</div>
+        </div>
+        <div className="singleDesign">
+          <img onLoad={this.onLoad} className="design" id="emcc" src="EMCC.jpg"/>
+          <div className="loading">loading mind-blowing creation...</div>
+        </div>
       </div>
-      <div className="singleDesign">
-        <img className="design" src="INTEMPO.jpg"/>
-      </div>
-      <div className="singleDesign">
-        <img className="design" src="DACAPO.jpg"/>
-      </div>
-      <div className="singleDesign">
-        <img className="design" src="EMCC.jpg"/>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Designs
