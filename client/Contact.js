c0c3bb32cf51bbaaee1a8d7b762dd2806e2e802e
Cheryl Ng <cherylngql@gmail.com>
2018-09-09 19:09:40 -0400

    responsive design

--- client/Contact.js
+++ client/Contact.js
@@ -51,24 +51,25 @@ class Contact extends Component {
   render() {
     return (
       <div id="contact">
-        <div className="extrashort">
-          <h3>I am interested in whatever you have to say</h3>
+        <div id="subcontact">
+          <h3>I am interested in whatever you have to say. Please leave me a note!</h3>
+          <div className="extrashort"/>
+          {
+            this.state.success ?
+            <p>Thank you for sharing!</p> :
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-element">
+                <label>Email</label>
+                <input name="email" value={this.state.email} onChange={this.handleChange}/>
+              </div>
+              <div className="form-element">
+                <textarea name="content" value={this.state.content} onChange={this.handleChange}/>
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          }
+          {this.state.errorMessage ? <p>{this.state.errorMessage}</p> : null}
         </div>
-        {
-          this.state.success ?
-          <p>Thank you for sharing!</p> :
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-element">
-              <label>Email</label>
-              <input name="email" value={this.state.email} onChange={this.handleChange}/>
-            </div>
-            <div className="form-element">
-              <textarea name="content" value={this.state.content} onChange={this.handleChange}/>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        }
-        {this.state.errorMessage ? <p>{this.state.errorMessage}</p> : null}
       </div>
     )
   }
--- client/NavBar.js
+++ client/NavBar.js
@@ -3,13 +3,23 @@ import {Link} from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div id="nav-container">
-      <Link to={'/'}><p className="home-link">Cheryl Ng</p><img id="icon" src="icon.png"/></Link>
-      <Link to={'/about'}><p className="nav-link">About Me</p></Link>
-      <Link to={'/projects'}><p className="nav-link">Projects</p></Link>
-      <Link to={'/designs'}><p className="nav-link">Designs</p></Link>
-      <Link to={'/contact'}><p className="nav-link">Contact</p></Link>
-    </div>
+    <React.Fragment>
+      <div id="nav-large">
+        <Link to={'/'}><p className="home-link">Cheryl Ng</p><img id="icon" src="icon.png"/></Link>
+        <Link to={'/about'}><p className="nav-link">About Me</p></Link>
+        <Link to={'/projects'}><p className="nav-link">Projects</p></Link>
+        <Link to={'/designs'}><p className="nav-link">Designs</p></Link>
+        <Link to={'/contact'}><p className="nav-link">Contact</p></Link>
+      </div>
+      <div id="nav-icon"></div>
+      <div id="nav-small">
+        <Link to={'/'}><p className="home-link">Cheryl Ng</p><img id="icon" src="icon.png"/></Link>
+        <Link to={'/about'}><p className="nav-link">About Me</p></Link>
+        <Link to={'/projects'}><p className="nav-link">Projects</p></Link>
+        <Link to={'/designs'}><p className="nav-link">Designs</p></Link>
+        <Link to={'/contact'}><p className="nav-link">Contact</p></Link>
+      </div>
+    </React.Fragment>
   )
 }
 
