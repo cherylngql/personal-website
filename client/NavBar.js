a25d0fb96e165b086baa2b89d2862efa24904dc4
Cheryl Ng <cherylngql@gmail.com>
2018-09-09 23:29:34 -0400

    fix navbar resizing bug

--- client/NavBar.js
+++ client/NavBar.js
@@ -34,7 +34,6 @@ class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <div id="nav-icon"><img src="menu-icon.png" onClick={this.handleClick}/></div>
         <div id="nav-container">
           <Link to={'/'}><p className="home-link">Cheryl Ng</p><img id="icon" src="icon.png"/></Link>
           <Link className="link" to={'/about'}><p className="nav-link">About Me</p></Link>
@@ -42,11 +41,14 @@ class NavBar extends Component {
           <Link className="link" to={'/designs'}><p className="nav-link">Designs</p></Link>
           <Link className="link" to={'/contact'}><p className="nav-link">Contact</p></Link>
         </div>
-        <div id="nav-container-small" onMouseLeave={this.handleMouseLeave}>
-          <Link className="link" onClick={this.handleClick} to={'/about'}><p className="nav-link">About Me</p></Link>
-          <Link className="link" onClick={this.handleClick} to={'/projects'}><p className="nav-link">Projects</p></Link>
-          <Link className="link" onClick={this.handleClick} to={'/designs'}><p className="nav-link">Designs</p></Link>
-          <Link className="link" onClick={this.handleClick} to={'/contact'}><p className="nav-link">Contact</p></Link>
+        <div id="nav-group-small">
+          <div id="nav-icon"><img src="menu-icon.png" onClick={this.handleClick}/></div>
+          <div id="nav-container-small" onMouseLeave={this.handleMouseLeave}>
+            <Link className="link" onClick={this.handleClick} to={'/about'}><p className="nav-link">About Me</p></Link>
+            <Link className="link" onClick={this.handleClick} to={'/projects'}><p className="nav-link">Projects</p></Link>
+            <Link className="link" onClick={this.handleClick} to={'/designs'}><p className="nav-link">Designs</p></Link>
+            <Link className="link" onClick={this.handleClick} to={'/contact'}><p className="nav-link">Contact</p></Link>
+          </div>
         </div>
       </React.Fragment>
     )
