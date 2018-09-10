78e07394807899e2a90f76761dddef03fc37e65c
Cheryl Ng <cherylngql@gmail.com>
2018-09-09 22:38:46 -0400

    responsive navbar

--- client/NavBar.js
+++ client/NavBar.js
@@ -1,26 +1,54 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {Component} from 'react'
+import {Link, withRouter} from 'react-router-dom'
 
-const NavBar = () => {
-  return (
-    <React.Fragment>
-      <div id="nav-large">
-        <Link to={'/'}><p className="home-link">Cheryl Ng</p><img id="icon" src="icon.png"/></Link>
-        <Link to={'/about'}><p className="nav-link">About Me</p></Link>
-        <Link to={'/projects'}><p className="nav-link">Projects</p></Link>
-        <Link to={'/designs'}><p className="nav-link">Designs</p></Link>
-        <Link to={'/contact'}><p className="nav-link">Contact</p></Link>
-      </div>
-      <div id="nav-icon"></div>
-      <div id="nav-small">
-        <Link to={'/'}><p className="home-link">Cheryl Ng</p><img id="icon" src="icon.png"/></Link>
-        <Link to={'/about'}><p className="nav-link">About Me</p></Link>
-        <Link to={'/projects'}><p className="nav-link">Projects</p></Link>
-        <Link to={'/designs'}><p className="nav-link">Designs</p></Link>
-        <Link to={'/contact'}><p className="nav-link">Contact</p></Link>
-      </div>
-    </React.Fragment>
-  )
+//Your initialization
+
+class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {expanded: false}
+    this.handleClick = this.handleClick.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+  }
+
+  handleClick(evt) {
+    if (!this.state.expanded) {
+      document.getElementById('nav-container-small').style.display = 'flex';
+      document.getElementById('nav-icon').style.display = 'none';
+      this.setState({expanded: true})
+    } else {
+      document.getElementById('nav-container-small').style.display = 'none';
+      document.getElementById('nav-icon').style.display = 'block';
+      this.setState({expanded: false})
+    }
+  }
+
+  handleMouseLeave(evt) {
+    document.getElementById('nav-container-small').style.display = 'none';
+      document.getElementById('nav-icon').style.display = 'block';
+      this.setState({expanded: false})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div id="nav-icon"><img src="menu-icon.png" onClick={this.handleClick}/></div>
+        <div id="nav-container">
+          <Link to={'/'}><p className="home-link">Cheryl Ng</p><img id="icon" src="icon.png"/></Link>
+          <Link className="link" to={'/about'}><p className="nav-link">About Me</p></Link>
+          <Link className="link" to={'/projects'}><p className="nav-link">Projects</p></Link>
+          <Link className="link" to={'/designs'}><p className="nav-link">Designs</p></Link>
+          <Link className="link" to={'/contact'}><p className="nav-link">Contact</p></Link>
+        </div>
+        <div id="nav-container-small" onMouseLeave={this.handleMouseLeave}>
+          <Link className="link" onClick={this.handleClick} to={'/about'}><p className="nav-link">About Me</p></Link>
+          <Link className="link" onClick={this.handleClick} to={'/projects'}><p className="nav-link">Projects</p></Link>
+          <Link className="link" onClick={this.handleClick} to={'/designs'}><p className="nav-link">Designs</p></Link>
+          <Link className="link" onClick={this.handleClick} to={'/contact'}><p className="nav-link">Contact</p></Link>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
-export default NavBar
+export default withRouter(NavBar)
